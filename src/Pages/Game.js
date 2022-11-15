867497af6903c43199cf31c36dee6933e5c90554
Jessica Dea <jessicardea@gmail.com>
2022-11-14 21:59:26 -0800

    Created choose difficulty function

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -2,9 +2,23 @@ import React from 'react';
 import '../App.css';
 
 function Game() {
+
+    function ChooseDifficulty() {
+        return (
+            <div>
+                <button>Easy</button>
+                <button>Medium</button>
+                <button>Hard</button>
+            </div>
+        )
+    }
+
     return (
         <div>
             Game
+            <div>
+                <ChooseDifficulty />
+            </div>
         </div>
     );
 }
