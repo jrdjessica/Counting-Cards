077de09a47eb394114dfbe90bc68e6757f38a170
Jessica Dea <jessicardea@gmail.com>
2022-11-14 22:13:36 -0800

    Modified GetCards function

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -5,20 +5,23 @@ import { cardDeck } from './Tutorial'
 function Game() {
     const [game, setGame] = React.useState('off');
 
+    const GameCards = []
+
     function ChooseDifficulty() {
         return (
             <div>
-                {game === 'off' && <button onClick={PlayGame}>Easy</button>}
-                {game === 'off' && <button onClick={PlayGame}>Medium</button>}
-                {game === 'off' && <button onClick={PlayGame}>Hard</button>}
+                {game === 'off' && <button onClick={GetCards(5)}>Easy</button>}
+                {game === 'off' && <button onClick={GetCards(15)}>Medium</button>}
+                {game === 'off' && <button onClick={GetCards(30)}>Hard</button>}
             </div>
         )
     }
 
-    function PlayGame() {
-        const keys = Object.keys(cardDeck)
-        const randomCard = keys[Math.floor(Math.random() * keys.length)]
-        setGame('on')
+    function GetCards(numCards) {
+        const keys = Object.keys(cardDeck);
+        const randomCard = keys[Math.floor(Math.random() * keys.length)];
+        setGame('on');
+        GameCards.append(randomCard);
     }
 
     return (
